f979e9a78aca9e80db0c6d36cf6cba22dd311831
B1GD3VD0G <devin.peevy@outlook.com>
2024-01-23 21:53:02 -0800

    Edited field and main loaders for village dimension. Set variable spawn points dependent upon LAST_ZONE.

--- js/scripts/zone_loaders/village_zone_loaders.js
+++ js/scripts/zone_loaders/village_zone_loaders.js
@@ -12,13 +12,33 @@ const loadVillageCanyon = () => {
 };
 
 const loadVillageField = () => {
-    const queueAssets = () => {
-
-    };
+    const queueAssets = () => {};
 
     const addEntities = () => {
+        // Add a border to the right side of the map, leading to the field.
+        GAME.addEntity(new Border(
+            new Vector(-1, 0), // start at the far right side of the Zone, and at the top
+            new Vector(1, ZONE.PIXEL_SIZE.y), // only one pixel wide, but as tall as the entire Zone.
+            Zone.getZones().village.main
+        ));
+        // Add a layer of blocks to the floor.
+        for (let x = ZONE.MIN_BLOCK.x; x <= ZONE.MAX_BLOCK.x; x++) {
+            GAME.addEntity(new Block(new Vector(x, ZONE.MAX_BLOCK.y), Block.GRASS));
+        }
 
+        if (LAST_ZONE.equals(Zone.getZones().village.mountain)) { // Coming from mountain.
+            // Set spawn point on the right.
+            const blockPos = new Vector(ZONE.MAX_BLOCK.x - 3, 20);
+            CHAD.pos = Vector.blockToWorldSpace(blockPos);
+        } else if (LAST_ZONE.equals(Zone.getZones().village.main)) { // Coming from main.
+            // spawn on left.
+            const blockPos = new Vector(1, 20);
+            CHAD.pos = Vector.blockToWorldSpace(blockPos);
+        }
     };
+
+    // Set background color:
+    BG_COLOR = "red";
 
     queueAssets();
     ASSET_MGR.downloadAll(addEntities);
@@ -38,20 +58,40 @@ const loadVillageInsideCave = () => {
 };
 
 const loadVillageMain = () => {
-    const queueAssets = () => {
-        ASSET_MGR.queueDownload(PapaChad.SPRITESHEET);
-        ASSET_MGR.queueDownload(Block.SPRITESHEET);
-    };
+    const queueAssets = () => {};
 
     const addEntities = () => {
+        // Add a border to the right side of the map, leading to the field.
+        GAME.addEntity(new Border(
+            new Vector(ZONE.MAX_PT.x, 0), // start at the far right side of the Zone, and at the top
+            new Vector(1, ZONE.PIXEL_SIZE.y), // only one pixel wide, but as tall as the entire Zone.
+            Zone.getZones().village.field
+        ));
         // Add a layer of blocks to the floor.
         for (let x = ZONE.MIN_BLOCK.x; x <= ZONE.MAX_BLOCK.x; x++) {
             GAME.addEntity(new Block(new Vector(x, ZONE.MAX_BLOCK.y), Block.DIRT));
         }
-        // Place chad above the blocks.
-        const blockPos = new Vector(5, 20);
-        CHAD.pos = Vector.blockToWorldSpace(blockPos);
+
+        // Place chad.
+
+        if (LAST_ZONE === null) { // We've just started the game.
+            // Spawn in middle.
+            const blockPos = new Vector(50, 20);
+            CHAD.pos = Vector.blockToWorldSpace(blockPos);
+
+        } else if (LAST_ZONE.equals(Zone.getZones().village.field)) { // Coming from field.
+            // Set spawn point on the right.
+            const blockPos = new Vector(ZONE.MAX_BLOCK.x - 3, 20);
+            CHAD.pos = Vector.blockToWorldSpace(blockPos);
+        } else if (LAST_ZONE.equals(Zone.getZones().village.outsideCave)) { // Coming from outside cave.
+            // spawn on left.
+            const blockPos = new Vector(1, 20);
+            CHAD.pos = Vector.blockToWorldSpace(blockPos);
+        }
     };
+
+    // Set Background Color:
+    BG_COLOR = COLORS.skyBlue;
 
     queueAssets();
     ASSET_MGR.downloadAll(addEntities);
