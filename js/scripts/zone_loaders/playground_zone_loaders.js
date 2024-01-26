6591e9d9c7e7a0d92fab37357728e226474fe76c
tclaar <122065716+tclaar@users.noreply.github.com>
2024-01-26 15:16:10 -0800

    Revert "Add basic combat."

--- js/scripts/zone_loaders/playground_zone_loaders.js
+++ js/scripts/zone_loaders/playground_zone_loaders.js
@@ -52,20 +52,12 @@ const loadPlaygroundNathan = () => {
 
 const loadPlaygroundTrae = () => {
     const queueAssets = () => {
-        ASSET_MGR.queueDownload(Snake.SPRITESHEET);
+
     };
 
     const addEntities = () => {
-        // Add a layer of blocks to the floor.
-        for (let x = ZONE.MIN_BLOCK.x; x <= ZONE.MAX_BLOCK.x; x++) {
-            GAME.addEntity(new Block(new Vector(x, ZONE.MAX_BLOCK.y), Block.DIRT));
-        }
-        GAME.addEntity(new Snake(Vector.blockToWorldSpace(new Vector(65, 20))));
 
-        CHAD.pos = Vector.blockToWorldSpace(new Vector(50, 20));
     };
-
-    BG_COLOR = "purple";
 
     queueAssets();
     ASSET_MGR.downloadAll(addEntities);
