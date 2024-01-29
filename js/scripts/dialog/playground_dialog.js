d22621d3a6c54e30705503b97b6f061dd3acef08
B1GD3VD0G <devin.peevy@outlook.com>
2024-01-28 17:37:52 -0800

    made a single conversation and an algorithm to exhaust it.

--- js/scripts/dialog/playground_dialog.js
+++ js/scripts/dialog/playground_dialog.js
@@ -1,5 +1,47 @@
-const loadPlaygroundDialog = () => {
-    const dialog = [];
-    dialog[0] = "Hello world!";
-    dialog[1] = "What's good?";
-};
+const CONVO1 = [
+    {//index 0
+        speaker:    "Papa Chad",
+        text:       "Good Morning son! How did you sleep?",
+        choices:  {
+            c1: {
+                text: "BAD",
+                next: 1
+            },
+            c2: {
+                text: "GOOD",
+                next: 2
+            }
+        }   
+    },
+    {//index 1
+        speaker:    "Papa Chad",
+        text:       "Go back to bed then son.",
+        end:        true
+    },
+    {// index 2
+        speaker:    "Papa Chad",
+        text:       "Let's go hunt some rabbits then!"
+    },
+    {// index 3
+        speaker:    "Chad",
+        text:       "Okay dad! GREAT!"
+    },
+    {// index 4
+        speaker:    null,
+        text:       "Use A and D to move right and left!",
+        end: true
+    }
+];
+
+// DialogEater(string[] convo)
+//  Set i = 0.
+//  while (i < convo.length):
+//      display convo[i].
+//      if (convo.responses):
+//          chosenResponse = [users choice of r1, r2, ...].
+//          i = chosenResponse.next.
+//      else if (!convo.end):
+//          i++.
+//      else:
+//          i = convo.length.
+//  End DialogEater.
