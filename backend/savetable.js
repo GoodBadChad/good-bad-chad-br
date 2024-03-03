3a4cfc57762817a4f54eb92923de732a627b3b89
B1GD3VD0G <devin.peevy@outlook.com>
2024-03-03 13:49:54 -0800

    attempt to use save functions in source code. doesn't work yet.

---
+++ backend/savetable.js
@@ -0,0 +1,11 @@
+const savetable = document.getElementById('savetable');
+
+savetable.innerHTML = '<h1>Save table:</h1>';
+
+// import('./functions');
+
+// test:
+// (async () => {
+//   const saves = await getSaves('test', 1);
+//   console.log(saves);
+// })();
